5728c1c4effc4327b967b9e0ea5d12c5e166e2a2
Sigurd <yujiang015@gmail.com>
2024-09-27 15:45:35 +0900

    Imported GhostSamurai_Bundle

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Sekirolike
+
+Sekirolike combat system made with Unreal Engine 5
+
+Animations from [GhostSamurai_Bundle
+](https://www.unrealengine.com/marketplace/en-US/product/ghostsamurai-bundle)
---
+++ Source/Sekirolike/Sekirolike.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Sekirolike.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Sekirolike, "Sekirolike" );
